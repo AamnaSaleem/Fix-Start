19c3470bc8bd706e323e01b17a717e74e50749a2
AamnaSaleem <93542393+AamnaSaleem@users.noreply.github.com>
2021-11-16 21:09:19 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim Str1 As String
+        Dim FirstChar As Char
+        Dim ThisChar As Char
+
+        Console.Write("Enter string to be processed: ")
+        Str1 = Console.ReadLine
+        FirstChar = Left(Str1, 1)
+        Console.Write(FirstChar)
+        For COUNT = 1 To Len(Str1) - 1
+            ThisChar = Mid(Str1, (COUNT + 1), 1)
+            If ThisChar = FirstChar Then
+                ThisChar = "*"
+            End If
+            Console.Write(ThisChar)
+        Next
+        Console.ReadKey()
+
+    End Sub
+
+End Module
